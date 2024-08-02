39942daf0f6e1315ab07f288ddb00c6a50f413ff
Paul Groves <paul@enginelabs.ai>
2024-08-02 09:25:25 +0100

    Remove name argument from runProcess

--- src/strategies/shellStrategy/toolFunctions/runProcess/runProcessFunctionDefinition.ts
+++ src/strategies/shellStrategy/toolFunctions/runProcess/runProcessFunctionDefinition.ts
@@ -8,21 +8,17 @@ const description = "Run a process in the project directory in the background";
 const parameters = {
   type: "object",
   properties: {
-    name: {
-      type: "string",
-      description: "The name of the process which you can use to stop it later",
-    },
     command: {
       type: "string",
       description:
         "The full shell command to run the process with flags e.g. `npm run dev`",
     },
   },
-  required: ["name", "command"],
+  required: ["command"],
 };
 
 async function run(params: any): Promise<string> {
-  runProcess(params.name, params.command);
+  runProcess(params.command);
 
   return `App is running`;
 }
