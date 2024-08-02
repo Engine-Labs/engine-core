fd702b1022e6b131bbb15ba104a2f6a85f5213a0
Paul Groves <paul@enginelabs.ai>
2024-08-02 09:25:25 +0100

    Remove name argument from runProcess

--- src/strategies/shellStrategy/toolFunctions/runProcess/runProcessFunctionAction.ts
+++ src/strategies/shellStrategy/toolFunctions/runProcess/runProcessFunctionAction.ts
@@ -1,10 +1,7 @@
 import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 import { logger, PROJECT_DIR } from "../../../../constants";
 
-export function runProcess(
-  name: string,
-  command: string
-): ChildProcessWithoutNullStreams {
+export function runProcess(command: string): ChildProcessWithoutNullStreams {
   const process = spawn(`/bin/bash`, ["-c", `. ~/.profile && ${command}`], {
     stdio: "pipe",
     detached: true,
@@ -12,21 +9,19 @@ export function runProcess(
     cwd: PROJECT_DIR,
   });
 
-  logger.debug(`Started ${name} process with PID: ${process.pid}`);
+  logger.debug(`Started subprocess with PID: ${process.pid}`);
 
   if (!process.pid) {
-    throw new Error(`Failed to start subprocess ${name}`);
+    throw new Error("Failed to start subprocess");
   }
 
   process.on("error", (err) => {
-    logger.error(`Failed to start subprocess ${name}: ${err.message}`);
-    throw new Error(`Failed to start subprocess ${name}: ${err.message}`);
+    logger.error(`Failed to start subprocess: ${err.message}`);
+    throw new Error(`Failed to start subprocess: ${err.message}`);
   });
 
   process.on("exit", (code, signal) => {
-    logger.debug(
-      `Subprocess ${name} exited with code ${code} and signal ${signal}`
-    );
+    logger.debug(`Subprocess exited with code ${code} and signal ${signal}`);
   });
 
   return process;
